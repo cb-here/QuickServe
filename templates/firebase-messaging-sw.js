87d59d968e2a5239b6511b1e88647707d61d8033
cb-here <cbv934@gmail.com>
2025-04-20 18:10:36 +0530

    firebase issue

--- templates/firebase-messaging-sw.js
+++ templates/firebase-messaging-sw.js
@@ -1,4 +1,4 @@
-pythimportScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js");
 
 const firebaseConfig = {
